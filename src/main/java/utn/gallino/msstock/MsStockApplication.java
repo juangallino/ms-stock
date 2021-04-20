cccac4197a4920999e517024a29651417f1a1b04
Your Name <you@example.com>
2021-04-20 19:51:32 -0300

    Initial commit de spring boot

---
+++ src/main/java/utn/gallino/msstock/MsStockApplication.java
@@ -0,0 +1,13 @@
+package utn.gallino.msstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStockApplication.class, args);
+	}
+
+}

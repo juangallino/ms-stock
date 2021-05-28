b97036f6e7b4e770cd0e6366f4ed514f44d0f990
Your Name <you@example.com>
2021-05-28 15:15:41 -0300

    system out console

--- src/main/java/utn/gallino/msstock/MsStockApplication.java
+++ src/main/java/utn/gallino/msstock/MsStockApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsStockApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MsStockApplication.class, args);
+		System.out.println("BUILD --SUCCESS");
+
 	}
 
 }

e64479d02361a3e51431dd80eeafd07a53b7bdcf
juangallino <juangallino>
2021-10-20 08:49:03 -0300

    Agregado Eureka

--- src/main/java/utn/gallino/msstock/MsStockApplication.java
+++ src/main/java/utn/gallino/msstock/MsStockApplication.java
@@ -2,13 +2,14 @@ package utn.gallino.msstock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJms
-
+@EnableEurekaClient
 public class MsStockApplication {
 
 	public static void main(String[] args) {
